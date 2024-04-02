73564f742bbe4f649cb0b63456e9c7d4c9c3caa8
matias <matiasmontalto@hotmail.com>
2024-04-02 16:17:26 -0300

    Segunda pre entrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 import { CartWidget } from "./CartWidget";
 
@@ -8,11 +9,11 @@ export const NavBar = () => {
     return(     
      <Navbar bg="primary" data-bs-theme="dark">
     <Container>
-      <Navbar.Brand href="#home">Almacen Carlitos</Navbar.Brand>
+      <Navbar.Brand to="" as={NavLink}>Almacen Carlitos</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="#home">Alimentos</Nav.Link>
-        <Nav.Link href="#features">Bebidas</Nav.Link>
-        <Nav.Link href="#pricing">Productos de limpieza</Nav.Link>
+        <Nav.Link to="/category/alimentos" as={NavLink}>Alimentos</Nav.Link>
+        <Nav.Link to="/category/bebidas" as={NavLink}>Bebidas</Nav.Link>
+        <Nav.Link to="/category/limpieza" as={NavLink}>Productos de limpieza</Nav.Link>
       </Nav>
       <CartWidget />
     </Container>
